3eb45df504fa4d1795f9f0289c12ea4c4062f9b6
masad-frost <farismasad@gmail.com>
2020-05-27 22:13:39 -0700

    Test cors for library

--- tests/suite/library.js
+++ tests/suite/library.js
@@ -1,12 +1,14 @@
 const { describe, it, before } = require('mocha');
 const { expect } = require('chai');
 const request = require('../base');
+const testCors = require('../cors');
 
 describe('/libraries/:library/:version', () => {
     describe('Requesting a valid library (:library = backbone.js)', () => {
         describe('Requesting a valid version (:version = 1.1.0)', () => {
             describe('No query params', () => {
-                const test = () => request().get('/libraries/backbone.js/1.1.0');
+                const path = '/libraries/backbone.js/1.1.0';
+                const test = () => request().get(path);
                 let response;
                 before('fetch endpoint', done => {
                     test().end((err, res) => {
@@ -14,6 +16,7 @@ describe('/libraries/:library/:version', () => {
                         done();
                     });
                 });
+                testCors(path, () => response);
                 it('returns the correct Cache headers', done => {
                     expect(response).to.have.header('Cache-Control', 'public, max-age=30672000, immutable'); // 355 days
                     done();
@@ -40,7 +43,8 @@ describe('/libraries/:library/:version', () => {
             });
 
             describe('Requesting a field (?fields=files)', () => {
-                const test = () => request().get('/libraries/backbone.js/1.1.0?fields=files');
+                const path = '/libraries/backbone.js/1.1.0?fields=files';
+                const test = () => request().get(path);
                 let response;
                 before('fetch endpoint', done => {
                     test().end((err, res) => {
@@ -48,6 +52,7 @@ describe('/libraries/:library/:version', () => {
                         done();
                     });
                 });
+                testCors(path, () => response);
                 it('returns the correct Cache headers', done => {
                     expect(response).to.have.header('Cache-Control', 'public, max-age=30672000, immutable'); // 355 days
                     done();
@@ -70,7 +75,8 @@ describe('/libraries/:library/:version', () => {
             });
 
             describe('Requesting all fields (?fields=*)', () => {
-                const test = () => request().get('/libraries/backbone.js/1.1.0?fields=*');
+                const path = '/libraries/backbone.js/1.1.0?fields=*';
+                const test = () => request().get(path);
                 let response;
                 before('fetch endpoint', done => {
                     test().end((err, res) => {
@@ -78,6 +84,7 @@ describe('/libraries/:library/:version', () => {
                         done();
                     });
                 });
+                testCors(path, () => response);
                 it('returns the correct Cache headers', done => {
                     expect(response).to.have.header('Cache-Control', 'public, max-age=30672000, immutable'); // 355 days
                     done();
@@ -106,7 +113,8 @@ describe('/libraries/:library/:version', () => {
 
         describe('Requesting a non-existent version (:version = this-version-doesnt-exist)', () => {
             describe('No query params', () => {
-                const test = () => request().get('/libraries/backbone.js/this-version-doesnt-exist');
+                const path = '/libraries/backbone.js/this-version-doesnt-exist';
+                const test = () => request().get(path);
                 let response;
                 before('fetch endpoint', done => {
                     test().end((err, res) => {
@@ -114,6 +122,7 @@ describe('/libraries/:library/:version', () => {
                         done();
                     });
                 });
+                testCors(path, () => response);
                 it('returns the correct Cache headers', done => {
                     expect(response).to.have.header('Cache-Control', 'public, max-age=3600'); // 1 hour
                     done();
@@ -132,7 +141,8 @@ describe('/libraries/:library/:version', () => {
 
     describe('Requesting a non-existent library (:library = this-library-doesnt-exist)', () => {
         describe('No query params', () => {
-            const test = () => request().get('/libraries/this-library-doesnt-exist');
+            const path = '/libraries/this-library-doesnt-exist';
+            const test = () => request().get(path);
             let response;
             before('fetch endpoint', done => {
                 test().end((err, res) => {
@@ -159,7 +169,8 @@ describe('/libraries/:library/:version', () => {
 describe('/libraries/:library', () => {
     describe('Requesting a valid library (:library = backbone.js)', () => {
         describe('No query params', () => {
-            const test = () => request().get('/libraries/backbone.js');
+            const path = '/libraries/backbone.js';
+            const test = () => request().get(path);
             let response;
             before('fetch endpoint', done => {
                 test().end((err, res) => {
@@ -241,7 +252,8 @@ describe('/libraries/:library', () => {
         });
 
         describe('Requesting a field (?fields=assets)', () => {
-            const test = () => request().get('/libraries/backbone.js?fields=assets');
+            const path = '/libraries/backbone.js?fields=assets';
+            const test = () => request().get(path);
             let response;
             before('fetch endpoint', done => {
                 test().end((err, res) => {
@@ -271,7 +283,8 @@ describe('/libraries/:library', () => {
         });
 
         describe('Requesting all fields (?fields=*)', () => {
-            const test = () => request().get('/libraries/backbone.js?fields=*');
+            const path = '/libraries/backbone.js?fields=*';
+            const test = () => request().get(path);
             let response;
             before('fetch endpoint', done => {
                 test().end((err, res) => {
@@ -314,7 +327,8 @@ describe('/libraries/:library', () => {
 
     describe('Requesting a non-existent library (:library = this-library-doesnt-exist)', () => {
         describe('No query params', () => {
-            const test = () => request().get('/libraries/this-library-doesnt-exist');
+            const path = '/libraries/this-library-doesnt-exist';
+            const test = () => request().get(path);
             let response;
             before('fetch endpoint', done => {
                 test().end((err, res) => {
