e9755a093d39de2f79a801d57a7fcf11f6f8bf68
MattIPv4 <me@mattcowley.co.uk>
2020-08-09 20:39:44 +0100

    Make tests wait for API server to start

--- tests/run.js
+++ tests/run.js
@@ -1,39 +1,62 @@
 const { spawn, spawnSync } = require('child_process');
 
+const kill = server => {
+    console.info('Killing API server...');
+    server.kill();
+    try {
+        process.kill(server.pid);
+    } catch (_) {
+        // This is a backup call, if it fails all is fine
+    }
+};
+
+const exit = code => {
+    console.info(`Exiting with exit code ${code}...`);
+    process.exit(code);
+};
+
+const mocha = server => {
+    // Run the tests
+    console.info('Starting API tests via mocha...');
+    const tests = spawnSync('npm', ['run', 'test:mocha']);
+
+    // Kill server
+    kill(server);
+
+    // Log test results
+    console.info('API tests results:');
+    console.log(`${tests.stdout}`.trim());
+    const err = `${tests.stderr}`.trim();
+    console.error(err);
+
+    // Exit
+    exit(err ? 1 : 0);
+};
+
 const main = () => {
     // Start the API server in the background
     console.info('Starting API server for testing...');
     const server = spawn('npm', ['run', 'dev']);
 
-    // Log server messages
-    server.stdout.on('data', data => `${data}`.trim().split('\n').map(line => console.log(`server: ${line}`)));
-    server.stderr.on('data', data => `${data}`.trim().split('\n').map(line => console.error(`server: error: ${line}`)));
+    // Set a 1 minute timeout for the server starting
+    const timeout = setTimeout(() => {
+        console.error('API server did not start in time, aborting...');
+        kill(server);
+    }, 3 * 60 * 1000);
 
-    // Give the server time to start (10s)
-    setTimeout(() => {
-        // Run the tests
-        console.info('Starting API tests via mocha...');
-        const tests = spawnSync('npm', ['run', 'test:mocha']);
+    server.stdout.on('data', data => {
+        // Log any stdout messages
+        `${data}`.trim().split('\n').map(line => console.log(`server: ${line}`));
 
-        // Kill server
-        console.info('Killing API server...');
-        server.kill();
-        try {
-            process.kill(server.pid);
-        } catch (_) {
-            // This is a backup call, if it fails all is fine
+        // Run the mocha tests if the server started
+        if (`${data}`.trim().startsWith('Listening on ')) {
+            clearTimeout(timeout);
+            mocha(server);
         }
+    });
 
-        // Log test results
-        console.info('API tests results:');
-        console.log(`${tests.stdout}`.trim());
-        const err = `${tests.stderr}`.trim();
-        console.error(err);
-
-        // Exit
-        console.info(`Exiting with exit code ${err ? 1 : 0}...`);
-        process.exit(err ? 1 : 0);
-    }, 10 * 1000);
+    // Log any stderr messages
+    server.stderr.on('data', data => `${data}`.trim().split('\n').map(line => console.error(`server: error: ${line}`)));
 };
 
 main();
