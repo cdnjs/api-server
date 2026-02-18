afa7ba907d35cd939bf3b5c78d3f7126b89a02a4
MattIPv4 <me@mattcowley.co.uk>
2026-02-18 01:00:34 +0000

    Set user-agent for test suite requests

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
 
 export default defineWorkersConfig({
     test: {
+        silent: 'passed-only',
         reporters: [
             'verbose',
             process.env.GITHUB_ACTIONS && 'github-actions',
