48cdc23ad465a8441cf959af7a6089bffafab11e
MattIPv4 <me@mattcowley.co.uk>
2026-02-28 21:36:26 +0000

    Fix + simplify filter typings

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,36 +1,12 @@
 /**
- * Escape a string for rendering in HTML.
- *
- * @param unsafe Unsafe string to escape.
- */
-const escape = (unsafe: string) => unsafe
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#039;');
-
-/**
- * Generate an object containing the given fields, with values from the given source object or null.
- *
- * Field names will be escaped to be made safe for HTML.
- * This is a legacy behaviour for custom fields being requests.
- * No standard fields have characters that would require escaping.
+ * Filter a given object by a set of field names, or all fields.
  *
  * @param source Source object to extract safe fields from.
  * @param fields Field names to include in generated object.
  * @param all Generate object with all fields from source object (ignores fields param).
  */
-export default (source: Record<string, unknown>, fields: string[], all = false) => {
-    if (all) {
-        return Object.entries(source).reduce((obj, [ key, value ]) => ({
-            ...obj,
-            [escape(key)]: value,
-        }), {});
-    }
+export default <T extends Record<string, unknown>>(source: T, fields: string[], all = false): Partial<T> => {
+    if (all) return source;
 
-    return fields.reduce((obj, field) => ({
-        ...obj,
-        [escape(field)]: source[field] ?? null,
-    }), {});
+    return fields.reduce((obj, field) => field in source ? { ...obj, [field]: source[field] } : obj, {} as Partial<T>);
 };
