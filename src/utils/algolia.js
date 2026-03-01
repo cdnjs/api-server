c943909a9f0f967f7efe18cfb12b0e8da7417b62
MattIPv4 <me@mattcowley.co.uk>
2026-03-01 15:34:09 +0000

    Remove custom requester

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -1,48 +1,10 @@
 import { algoliasearch } from 'algoliasearch';
 
 /**
- * Custom requester for Algolia using fetch.
+ * Create an Algolia SearchClient for the cdnjs app.
  *
- * @type {import('@algolia/client-common').Requester}
- */
-const requester = {
-    /**
-     * Send a request to the Algolia API.
-     *
-     * @param {import('@algolia/client-common').EndRequest} req Request to send.
-     * @return {Promise<import('@algolia/client-common').Response>}
-     */
-    send: req => {
-        // Create the timeout
-        const hasTimeout = typeof req.responseTimeout === 'number' && req.responseTimeout > 0;
-        const controller = hasTimeout && new AbortController();
-        const timer = hasTimeout && setTimeout(() => controller.abort(), req.responseTimeout * 1000);
-
-        // Send the request
-        return fetch(req.url, {
-            method: req.method,
-            headers: req.headers,
-            body: req.data,
-            ...(hasTimeout
-                ? { signal: controller.signal }
-                : {}),
-        }).then(async resp => ({
-            content: await resp.text().catch(() => ''),
-            isTimedOut: false,
-            status: resp.status,
-        })).catch(err => ({
-            content: '',
-            isTimedOut: err.name === 'AbortError',
-            status: -1,
-        })).finally(() => {
-            if (timer) clearTimeout(timer);
-        });
-    },
-};
-
-/**
- * Create an Algolia SearchClient for the cdnjs app using a custom fetch requester.
+ * @see https://github.com/algolia/algoliasearch-client-javascript/blob/main/packages/client-search/builds/fetch.ts
  *
  * @return {import('algoliasearch').Algoliasearch}
  */
-export default () => algoliasearch('2QWLVLXZB6', 'e16bd99a5c7a8fccae13ad40762eec3c', { requester });
+export default () => algoliasearch('2QWLVLXZB6', 'e16bd99a5c7a8fccae13ad40762eec3c');
