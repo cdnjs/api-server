6a0dda80b2da8f591fac9c81b815f5c1d31bfa74
MattIPv4 <me@mattcowley.co.uk>
2026-03-08 23:44:39 +0000

    Add eslint config for prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,17 @@ import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import jsdoc from 'eslint-plugin-jsdoc';
+import prettier from 'eslint-config-prettier';
 
 export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.strict,
     tseslint.configs.stylistic,
-    jsdoc.configs['flat/recommended-typescript'],
+    jsdoc.configs['flat/recommended-mixed'],
+    {
+        name: 'prettier/config',
+        ...prettier,
+    },
     {
         name: 'custom/jsdoc',
         rules: {
