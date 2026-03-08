ab99ad59ef99cbf22a433404a5986889162b7404
MattIPv4 <me@mattcowley.co.uk>
2026-03-08 23:49:47 +0000

    Add import order for prettier

--- prettier.config.js
+++ prettier.config.js
@@ -5,5 +5,10 @@
  * @type {import('prettier').Config}
  */
 export default {
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
     singleQuote: true,
+    importOrder: ['<THIRD_PARTY_MODULES>', '^\\.\\.', '^\\.'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrderSideEffects: false,
 };
