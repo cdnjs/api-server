dc357861ccd9eaf9cae0233bfe7b49906ab8c27e
MattIPv4 <me@mattcowley.co.uk>
2026-08-16 17:49:00 +0100

    Set custom reporters for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
     testDir: './src',
     testMatch: '**/*.browser.spec.ts',
-    outputDir: '.wrangler/playwright-results',
+    outputDir: '.playwright/results',
+    reporter: [
+        ['list', { printSteps: true }],
+        ['html', { outputFolder: '.playwright/report' }],
+        ...(process.env.GITHUB_ACTIONS ? [['github'] as const] : []),
+    ],
     workers: 1,
     timeout: 30_000,
     expect: {
