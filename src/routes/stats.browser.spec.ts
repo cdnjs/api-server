221d73373506534638f0083186d6c71271075901
MattIPv4 <me@mattcowley.co.uk>
2026-08-16 22:39:06 +0100

    Add browser coverage for /stats

---
+++ src/routes/stats.browser.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '../utils/spec/playwright.ts';
+
+test.describe('/stats', () => {
+    test('error response', async ({ page }) => {
+        const response = await page.goto('/stats');
+        expect(response?.ok()).toBe(false);
+        expect(response?.status()).toBe(404);
+        expect(response?.headers()['cache-control']).toBe(
+            'public, max-age=3600',
+        ); // 1 hour
+
+        await expect(page).toHaveTitle('Endpoint Not Found - cdnjs');
+        await expect(page.locator('link[rel="canonical"]')).toHaveAttribute(
+            'href',
+            'https://cdnjs.com/stats',
+        );
+    });
+});
